91faed3ee211edf9517e80ef622b54ed4eedea18
Enrico <enrico@aira.io>
2019-12-02 22:22:06 -0800

    Minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 extern crate serde;
 extern crate serde_json;
+extern crate indicatif;
+extern crate rayon;
+
+
+use std::fs;
 
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
+use indicatif::ProgressIterator;
+use serde_json::Value;
+use std::prelude::v1::Vec;
+
+const TRANSCRIPTS_DIR: &str = "../../raw_transcripts";
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -30,5 +40,19 @@ pub struct Words {
 
 
 fn main() {
-    println!("Hello, world!");
+    let paths = fs::read_dir(TRANSCRIPTS_DIR).unwrap();
+    let mut transcripts: Vec<String> = Vec::new();
+
+    for path in paths.progress() {
+        let transcript = fs::read_to_string(path.unwrap().path()).unwrap();
+//        println!("{}", transcript);
+        transcripts.push(transcript);
+
+        // Parse the string of data into serde_json::Value.
+//        let v: Value = serde_json::from_str(data)?;
+
+//        println!("{}", path.unwrap().path().display())
+    }
+
+    println!("Loaded {} transcripts!", transcripts.len())
 }
